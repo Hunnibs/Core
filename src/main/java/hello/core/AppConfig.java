3af2ead4f7b2a17ae1e843869b8dc27ef3134ee1
Hunnibs <cqqudgjs@hufs.ac.kr>
2023-01-23 23:40:08 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,6 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,10 +10,18 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private MemoryMemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+    }
+
+    private DiscountPolicy DiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
