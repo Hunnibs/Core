b8824ac650aea1bd5f35e0586ad7e3b83589c95c
Hunnibs <cqqudgjs@hufs.ac.kr>
2023-02-16 15:27:27 +0900

    lombok

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
-
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
     private int FixDiscount = 1000; // 고정 할인 금액: 1000원
 
